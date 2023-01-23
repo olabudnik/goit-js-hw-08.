2622c04a54c88ab9440887907b06713ece68c7cc
olabudnik <olyabudnik98@gmail.com>
2023-01-23 18:05:56 +0100

    homework

---
+++ src/js/03-feedback.js
@@ -0,0 +1,60 @@
+// const refs = {
+//     form: document.querySelector('.feedback-form')
+// }
+// const STORAGE_KEY = 'feedback-form-state'
+// let formData = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
+
+// refs.form.addEventListener('input', onFormInput); 
+// refs.form.addEventListener('submit', onFormSubmit);
+
+// initForm();
+
+// function onFormInput(event){
+//  formData[event.target.name] = event.target.value;
+//  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+// }
+
+// function onFormSubmit(event){
+// event.preventDefault();
+// localStorage.removeItem(STORAGE_KEY);
+// event.target.reset();
+// formData = {}; 
+// }
+
+// function initForm() {
+//     const initValues = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+//     if (initValues) {
+//  const{email, message} = refs.form.elements; 
+//  email.value = initValues.email || ''; 
+//  email.value = initValues.message|| ''; 
+// }
+// }
+import throttle from 'lodash.throttle';
+const refs = {
+    form: document.querySelector('.feedback-form')
+}
+const STORAGE_KEY = 'feedback-form-state';
+let formData = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
+refs.form.addEventListener('input', throttle(onFormInput, 500));
+refs.form.addEventListener('submit', onFormSubmit);
+console.log(onFormInput);
+initForm();
+function onFormInput(event) {
+    formData[event.target.name] = event.target.value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+function onFormSubmit(event) {
+    event.preventDefault();
+    localStorage.removeItem(STORAGE_KEY);
+    event.target.reset();
+    formData = {};
+}
+function initForm() {
+    const initValues = JSON.parse(localStorage.getItem(STORAGE_KEY));
+    if (initValues) {
+        const { email, message } = refs.form.elements;
+        email.value = initValues.email || '';
+        message.value = initValues.message || '';
+    };
+}
